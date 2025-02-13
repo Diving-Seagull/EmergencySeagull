bed137543f5fa4f9a50a258f19019b07d4864b52
J-1ac <tkdwns26@naver.com>
2025-02-13 10:28:58 +0900

    feat: 음성신고시 텍스트 분류 기능 추가

--- BE/EmergencySeagull/src/main/java/EmergencySeagull/gpt/dto/ClassificationRequest.java
+++ BE/EmergencySeagull/src/main/java/EmergencySeagull/gpt/dto/ClassificationRequest.java
@@ -1,11 +1,14 @@
 package EmergencySeagull.gpt.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import java.util.List;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ClassificationRequest {
 
     private String model;
--- BE/EmergencySeagull/src/main/java/EmergencySeagull/gpt/dto/TranscriptionRequest.java
+++ BE/EmergencySeagull/src/main/java/EmergencySeagull/gpt/dto/TranscriptionRequest.java
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
-@AllArgsConstructor
+@NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class TranscriptionResponse {
+@AllArgsConstructor
+public class TranscriptionRequest {
 
-    private String text;
+    private double latitude;
+    private double longitude;
 }
