59f0621527e6533c5279b3e135590e8731a15b4c
gykim_kr <k_bright_st@pusan.ac.kr>
2025-02-17 19:37:29 +0900

    feat: User UI 및 녹음 애니메이션, 카카오 지도 연결. Author: KimGiyun

--- FE/app/src/main/java/com/divingseagull/emergencyseagull/MainActivity.kt
+++ FE/app/src/main/java/com/divingseagull/emergencyseagull/MainActivity.kt
@@ -3,6 +3,7 @@ package com.divingseagull.emergencyseagull
 import android.Manifest
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,6 +12,9 @@ import com.divingseagull.emergencyseagull.composable.AndroidAudioPlayer
 import com.divingseagull.emergencyseagull.composable.AndroidAudioRecorder
 import com.divingseagull.emergencyseagull.navigation.NavGraph
 import com.divingseagull.emergencyseagull.ui.theme.EmergencySeagullTheme
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
+import com.kakao.vectormap.KakaoMapSdk
 
 class MainActivity : ComponentActivity() {
     private val recorder by lazy {
@@ -24,11 +28,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         ActivityCompat.requestPermissions(
             this,
-            arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            arrayOf(Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
             0
         )
         enableEdgeToEdge()
         setContent {
+            Log.d("KeyHash", "${Utility.getKeyHash(this)}")
+
+            val kakaoMapKey = BuildConfig.KAKAO_MAP_KEY
+            KakaoSdk.init(this, kakaoMapKey)
+            KakaoMapSdk.init(this, kakaoMapKey);
             EmergencySeagullTheme {
                 NavGraph(recorder, player)
             }
